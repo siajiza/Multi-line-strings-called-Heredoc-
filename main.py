cc51b2a102831c9e42a9778fba7649d8a75787ad
Siajiza <>
2021-04-08 06:23:47 +0000

    Add strip() only into the content_01

---
+++ main.py
@@ -0,0 +1,26 @@
+# Heredoc 01
+
+content_01 = """
+TITLE 01
+Content 01 start here. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet
+""".strip()
+
+# Heredoc 02
+
+content_02 = """
+TITLE 02
+Content 02 start here. Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+"""
+
+print(content_01)
+
+print(content_02)
+
